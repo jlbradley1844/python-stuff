73a3b3559cd9b7e9860ef7d43dfe6de722a31e85
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-11-01 02:13:06 +0000

    fixes

--- bahai_writings/draft.py
+++ bahai_writings/draft.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python
 import spacy
+import re
+
+import pdb
 
 # read in the text file you wish to analyze
-with open('gleanings.txt', 'r') as myfile:
+with open('texts/gleanings.txt', 'r') as myfile:
     text=myfile.read()
 # set up spacy
 import spacy
@@ -14,3 +18,42 @@ doc = nlp.tokenizer(utext)
 nlp.tagger(doc)
 nlp.parser(doc)
 nlp.entity(doc)
+
+def get_headers(doc_string):
+    """
+    Headers in these documents are generally all-caps. That gives an indication
+    of where the spans are. It will take the document as a string and return
+    an array of tuples. Each tuple will be beginning and end character position
+    of a header. len() of the returned array is the number of sections.
+    """
+    header=re.compile("(\r\n|^)[^a-z\r\n]+(\r\n|$)")
+    matches=header.search(doc_string)
+    header_pos = []
+    tail=0
+    while matches is not None:
+        header_pos.append((tail + matches.span()[0], tail + matches.span()[1]))
+        tail = tail + matches.span()[1]
+        matches=header.search(doc_string[tail:])
+
+    return header_pos
+
+def get_paragraph_markers(doc_string):
+    """
+    Gets position of the paragraph breaks. A paragraph is at least two line feeds
+    in a row
+    """
+    para=re.compile("\r\n(\r\n)+")
+    matches=para.search(doc_string)
+    marker_pos = []
+    tail=0
+    while matches is not None:
+        marker_pos.append((tail + matches.span()[0], tail + matches.span()[1]))
+        tail = tail + matches.span()[1]
+        matches=para.search(doc_string[tail:])
+
+    return marker_pos
+    
+foo1=get_headers(utext)
+foo2=get_paragraph_markers(utext)
+print foo1
+print foo2
